2e765b93f5854ac573a3b75b2b94354790af7f35
Ashleigh <ashleighmaund@gmail.com>
2015-05-11 14:28:05 +0100

    added image to '/' page

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  "hello!"
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+get '/page_of_secrets' do
+  "This is where the basilisk lives..."
 end
